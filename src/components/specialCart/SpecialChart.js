fc34e9e0385fa1f2db1dc12b65ed2fc17707ba86
hasan-ullah <mdhasanullah124@gmail.com>
2022-03-28 13:06:54 +0600

    data loade by axios and use barchart by making data key forcely

--- src/components/specialCart/SpecialChart.js
+++ src/components/specialCart/SpecialChart.js
@@ -1,17 +1,34 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { Bar, BarChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const SpecialChart = () => {
     const [phones,setPhones]=useState([])
 
     useEffect(()=>{
        axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
-       .then(data=>console.log(data.data))
+       .then(data=>{
+           const loadedData= data.data.data;
+           const phoneData = loadedData.map(phone=>{
+               const parts = phone.slug.split('-');
+               const ph={
+                   name:parts[0],
+                   value:parseInt(parts[1])
+               };
+               return ph;
+           })
+           setPhones(phoneData)
+           console.log(phoneData)
+
+       })
     },[])
     return (
-        <div>
-            
-        </div>
+        <BarChart width={600} height={400} data={phones}>
+        <Bar dataKey="value" fill="#8884d8" />
+        <XAxis dataKey={"name"} ></XAxis>
+        <YAxis ></YAxis>
+        <Tooltip />
+      </BarChart>
     );
 };
 
