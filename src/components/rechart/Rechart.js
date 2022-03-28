203e610f9ff18231d378d944f3beff9f6aa1032b
hasan-ullah <mdhasanullah124@gmail.com>
2022-03-28 11:59:44 +0600

    making a line chart

--- src/components/rechart/Rechart.js
+++ src/components/rechart/Rechart.js
@@ -1,55 +1,59 @@
 import React from 'react';
-import { Line, LineChart } from 'recharts';
+import { Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Rechart = () => {
     const data = [
         {
           name: 'suplier A',
           price: 4000,
-          pv: 2400,
+          salse: 2400,
           amt: 2400,
         },
         {
           name: 'suplier B',
           price: 3000,
-          pv: 1398,
+          salse: 1398,
           amt: 2210,
         },
         {
           name: 'suplier C',
           price: 2000,
-          pv: 9800,
+          salse: 9800,
           amt: 2290,
         },
         {
           name: 'suplier D',
           price: 2780,
-          pv: 3908,
+          salse: 3908,
           amt: 2000,
         },
         {
           name: 'suplier E',
           price: 1890,
-          pv: 4800,
+          salse: 4800,
           amt: 2181,
         },
         {
           name: 'suplier F',
           price: 2390,
-          pv: 3800,
+          salse: 3800,
           amt: 2500,
         },
         {
           name: 'suplier G',
           price: 3490,
-          pv: 4300,
+          salse: 4300,
           amt: 2100,
         },
       ];
 
     return (
-      <LineChart width={400} height={400} data={data}>
+      <LineChart width={600} height={400} data={data}>
           <Line dataKey={'price'}></Line>
+          <Line dataKey={'salse'}></Line>
+          <Tooltip />
+          <XAxis dataKey={'name'} />
+          <YAxis />
       </LineChart>
     );
 };
