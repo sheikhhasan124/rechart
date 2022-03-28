51e005076decd9d7568594478339ec8ae8d5fb7f
hasan-ullah <mdhasanullah124@gmail.com>
2022-03-28 15:27:14 +0600

    react sprinc work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Rechart from './components/rechart/Rechart';
 import SpecialChart from './components/specialCart/SpecialChart';
 import { useSpring, animated } from 'react-spring'
 
 const App = () => {
-    const props = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } })
+    const [flip, set] = useState(false)
+    const props = useSpring({ to: { opacity: 1 },
+                    from: { opacity: 0 },
+                    reset: true,
+                    reverse: flip,
+                    delay: 200,
+                    
+                    onRest: () => set(!flip),
+         })
     return (
         <div>
             <animated.div style={props}>I will fade in</animated.div>
